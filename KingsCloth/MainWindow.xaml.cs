fffc1973c9980f485a726a9a643a0a89a25cf613
Rinat Sabirzhanov <nicken1898@gmail.com>
2022-04-01 19:12:02 +0300

    TitleBarAdd

--- KingsCloth/MainWindow.xaml.cs
+++ KingsCloth/MainWindow.xaml.cs
@@ -45,5 +45,23 @@ namespace KingsCloth
         {
 
         }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnRestore_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
     }
 }
---
+++ KingsCloth/ViewModel/DiscoveryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KingsCloth
+{
+    class DiscoveryViewModel
+    {
+    }
+}
